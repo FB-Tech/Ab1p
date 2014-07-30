b44a98ef03502c3e3a03d1017027315887b42d8b
Chris Frerking <Chrisfrerking@live.com>
2014-07-29 20:41:20 -0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,1 @@
+var levelBackgrounds = ["backs\/crop53cffbd6e0bad.png","backs\/crop53cffbbd99abb.png","backs\/crop53cffbe832e3a.jpg","backs\/crop53cffbf03f2bd.jpg","backs\/crop53cffbfdcd48e.jpg","backs\/crop53cffc0a808b9.jpg","backs\/crop53cffc163894c.png","backs\/crop53cffc26442e3.jpg","backs\/crop53cffc31a5c46.jpg"]
